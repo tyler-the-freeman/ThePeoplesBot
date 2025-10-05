6aec6a833f9d1056796649f5c608c81a30754015
tyler-the-freeman <>
2025-10-05 01:46:08 -0500

    implement youtubejs to replace ytdl-core after it was broken yet again and abandoned

--- src/services/AudioSourceFactory.js
+++ src/services/AudioSourceFactory.js
@@ -3,11 +3,6 @@ import { YouTubeAudioSource } from './sources/YouTubeAudioSource.js';
 
 export class AudioSourceFactory {
     static createSource(url) {
-        // if (url.includes('youtube.com') || url.includes('youtu.be')) {
-        //     return new YouTubeAudioSource(url);
-        // }
-
-        // throw new Error('Unsupported audio source');
         return new YouTubeAudioSource(url);
     }
 }
